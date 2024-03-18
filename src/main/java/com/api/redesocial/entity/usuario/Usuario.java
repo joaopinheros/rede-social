e5d94591f6e41d3b3517841f242181a5cee2e509
João <joaov.pinheiro01@gmail.com>
2024-03-18 15:32:54 -0300

    construção da classe de autenticação e token

--- src/main/java/com/api/redesocial/entity/usuario/Usuario.java
+++ src/main/java/com/api/redesocial/entity/usuario/Usuario.java
@@ -2,7 +2,12 @@ package com.api.redesocial.entity.usuario;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Getter
@@ -13,21 +18,61 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "usuario")
-public class Usuario {
+public class Usuario implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
-    private String email;
+    private String login;
     private String senha;
 
     @Enumerated
     private Roles role;
 
-    public Usuario (String email, String senha, Roles role){
-        this.email = email;
+    public Usuario (String login, String senha, Roles role){
+        this.login = login;
         this.senha = senha;
         this.role = role;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (this.role == Roles.ADMIN){
+            return List.of(new SimpleGrantedAuthority("ROLE_ADMIN"),
+                    new SimpleGrantedAuthority("ROLE_USER"));
+        }else {
+            return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+        }
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
     }
 }
--- src/main/java/com/api/redesocial/repository/UsuarioRepository.java
+++ src/main/java/com/api/redesocial/repository/UsuarioRepository.java
@@ -2,8 +2,11 @@ package com.api.redesocial.repository;
 
 import com.api.redesocial.entity.usuario.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.UUID;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, UUID> {
+
+    UserDetails findByLogin(String login);
 }
