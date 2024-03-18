ad84c64309477084d1c71fc8e0b6df1af3b438d1
João <joaov.pinheiro01@gmail.com>
2024-03-17 21:29:29 -0300

    Alterações no projeto e criei o db de notificação

--- src/main/java/com/api/redesocial/entity/usuario/Perfil.java
+++ src/main/java/com/api/redesocial/entity/usuario/Perfil.java
@@ -24,7 +24,6 @@ public class Perfil {
     private String nome;
     private String biografia;
     private String foto;
-    private String avatar;
 
     @OneToOne
     @JoinColumn(name = "usuario_id")
--- src/main/resources/db/migration/V01__perfil.sql
+++ src/main/resources/db/migration/V01__perfil.sql
@@ -3,7 +3,6 @@ CREATE TABLE perfil(
     nome VARCHAR(50) NOT NULL,
     biografia VARCHAR(100),
     foto VARCHAR(255),
-    avatar VARCHAR(255),
     usuario_id UUID,
     FOREIGN KEY (usuario_id) REFERENCES usuario(id)
 );
